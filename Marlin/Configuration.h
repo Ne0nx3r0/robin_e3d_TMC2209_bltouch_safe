218c884eb76f28edc7ac79dc7a280dc7b918af5a
Ne0nx3r0 <Ne0nx3r0@gmail.com>
2022-02-06 11:26:53 -0600

    fix steps on extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Linda ev2dd"
+#define CUSTOM_MACHINE_NAME "Jimmy Jr SV1"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -747,7 +747,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 100, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
